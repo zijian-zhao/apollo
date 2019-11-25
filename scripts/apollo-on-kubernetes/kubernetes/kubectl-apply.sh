5b07846973cd0467d644fb1220e02ec4cf1e60bc
zijian-zhao <zijian.zhao@redteamobile.com>
2019-11-25 18:01:51 +0800

    适配k8s

--- scripts/apollo-on-kubernetes/kubernetes/kubectl-apply.sh
+++ scripts/apollo-on-kubernetes/kubernetes/kubectl-apply.sh
@@ -2,7 +2,7 @@
 kubectl create namespace sre
 
 # dev-env
-kubectl apply -f apollo-env-dev/service-mysql-for-apollo-dev-env.yaml --record && \
+#kubectl apply -f apollo-env-dev/service-mysql-for-apollo-dev-env.yaml --record && \
 kubectl apply -f apollo-env-dev/service-apollo-config-server-dev.yaml --record && \
 kubectl apply -f apollo-env-dev/service-apollo-admin-server-dev.yaml --record
 
